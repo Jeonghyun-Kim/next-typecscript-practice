53c0fb0eb0fd22cf8b986e8e342fae59ad069b8e
Kay Kim <kimjh@bawi.org>
2020-07-31 17:55:47 +0900

    .

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,4 +1,12 @@
 declare module 'next-translate/useTranslation' {
-  const useTranslation: any;
+  const useTranslation: {
+    t: any;
+    lang: any;
+  };
   export default useTranslation;
+}
+
+declare module 'next-translate/Link' {
+  const Link: any;
+  export default Link;
 }
