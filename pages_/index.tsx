824db6b2bb7038ffb670085376b6098f3b34e537
Kay Kim <kay@Kays-MacBook-Pro.local>
2020-08-13 09:36:57 +0900

    refine index page

--- pages_/index.tsx
+++ pages_/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import styled from 'styled-components';
 
 import useUser from '../lib/hooks/useUser';
 import TextLink from '../components/TextLink';
+
+import fetcher from '../lib/fetcher';
 
 const Title = styled.h1`
   font-size: 50px;
@@ -10,16 +13,34 @@ const Title = styled.h1`
 `;
 
 export default function Home() {
-  const { user } = useUser();
+  const { user, mutateUser } = useUser();
+  const router = useRouter();
 
   return (
     <>
       <Title>onDisplay</Title>
       <TextLink href="/about">About</TextLink>
-      <TextLink href="/login">Login</TextLink>
-      <TextLink href="/signup">Signup</TextLink>
+      {user && user.isLoggedIn ? (
+        <a
+          href="/api/logout"
+          onClick={async (e) => {
+            e.preventDefault();
+            await mutateUser(fetcher('/api/logout'));
+            router.push('/login');
+          }}
+        >
+          Logout
+        </a>
+      ) : (
+        <>
+          <TextLink href="/login">Login</TextLink>
+          <TextLink href="/signup">Signup</TextLink>
+        </>
+      )}
       <TextLink href="/profile">Profile</TextLink>
-      <TextLink href="/artwork/upload">Upload</TextLink>
+      {user && user.isArtist && (
+        <TextLink href="/artwork/upload">Upload</TextLink>
+      )}
       {user && user.admin && (
         <TextLink href="/admin">Admin</TextLink>
       )}
